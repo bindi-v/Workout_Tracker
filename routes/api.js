5e653808f96e26c08e75c109e629ec6a5e5d0986
bindi-v <bindi.vaghela@gmail.com>
2021-11-05 23:31:44 -0400

    modified routes

--- routes/api.js
+++ routes/api.js
@@ -11,19 +11,8 @@ router.post("/api/workouts", ({ body }, res) => {
     });
 });
 
-router.post("/api/workouts/range", ({ body }, res) => {
-  Workout.insertMany(body)
-    .then(dbWorkout => {
-      res.json(dbWorkout);
-    })
-    .catch(err => {
-      res.status(400).json(err);
-    });
-});
-
 router.get("/api/workouts", (req, res) => {
   Workout.find({})
-    .sort({ date: -1 })
     .then(dbWorkout => {
       res.json(dbWorkout);
     })
@@ -32,4 +21,39 @@ router.get("/api/workouts", (req, res) => {
     });
 });
 
+router.get("/api/workouts/range", (req, res) => {
+  Workout.find({})
+    .limit(7)
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+router.put("/api/workouts/:id", ({params, body}, res) => {
+  Workout.findOneAndUpdate(
+    {
+      _id: params.id
+    },
+    {
+      $push: {
+        exercises: body
+      }
+    },
+    {
+      new: true
+    })
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+      //  title: req.body.title,
+       // note: req.body.note,
+       // modified: Date.now()
+     // }
+    //},
+    .catch((err) => {
+        res.json(err);
+    });
+});
 module.exports = router;
--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useUnifiedTopology: true,
-   useNewUrlParser: true,
+  useNewUrlParser: true,
   useFindAndModify: false
 });
 
