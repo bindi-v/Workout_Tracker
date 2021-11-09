fdbdbfb4ee092001ffd40bb65b9e031ad0b97940
bindi-v <bindi.vaghela@gmail.com>
2021-11-09 15:30:11 -0500

    deploying to Heroku

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(express.static("public"));
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
    useUnifiedTopology: true,
    useNewUrlParser: true,
-  // useFindAndModify: false
+   useFindAndModify: false
 });
 
 // routes
